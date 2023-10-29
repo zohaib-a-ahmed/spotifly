e6d627130f68ba6a33aeaa3caa5aa55a088ec090
Zohaib Ahmed <zohaib0718@gmail.com>
2023-10-29 18:26:05 -0500

    working wrapped

--- src/components/Dash.tsx
+++ src/components/Dash.tsx
@@ -1,58 +1,55 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../supaBaseClient';
-import { Button, Center, VStack, Text } from '@chakra-ui/react';
+import Wrapped from './Wrapped';
+import {Center, VStack} from '@chakra-ui/react';
 
 const Dash = () => {
   const [accessToken, setAccessToken] = useState('');
   const [providerToken, setProviderToken] = useState<string | null | undefined>(null);
+  const [wrappedData, setWrappedData] = useState(null);  // New state to hold the wrapped data
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       async (event, session) => {
         console.log(`Supabase auth event: ${event}`);
         if (session) {
-          setAccessToken(session.access_token);  // set access token to state
+          setAccessToken(session.access_token);
           setProviderToken(session.provider_token);
         }
       }
     );
-    // Cleanup subscription
     return () => {
       authListener.subscription.unsubscribe();
     };
   }, []);
 
-  const handleClick = async () => {
-      if (!accessToken) {
-        console.error('User is not logged in');
-        return;
-      }
-      try {
-        const { data} = await supabase.functions.invoke('wrapped', {
-          body: { name: 'ooga booga',
-        access: providerToken },
-          headers: { Authorization: `Bearer ${accessToken}` },
-        })
-        console.log(data)
-    } catch (error){
-      console.log('ERROR:', error)
-    };
-  }
+  useEffect(() => {
+    if (providerToken && accessToken) {
+      fetchData();
+    }
+  }, [providerToken, accessToken]);
 
-    return (
-      <>
-        <Center>
-          <VStack spacing={8} position="absolute" top='10%'>
-            <Text fontSize={['4xl', '6xl', "9xl"]} fontWeight="bold" color='green'>
-              This is the dash
-            </Text>
-            <Button onClick={handleClick}>
-              Invoke Edge Function
-            </Button>
-          </VStack>
-        </Center>
-      </>
-    );
+  const fetchData = async () => {
+    try {
+      const { data } = await supabase.functions.invoke('wrapped', {
+        body: { access: providerToken },
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+      setWrappedData(data);  // Set the fetched data to state
+    } catch (error) {
+      console.log('ERROR:', error);
+    }
   };
+
+  return (
+    <>
+      <Center>
+        <VStack spacing={8} position="absolute" top='10%' width='100%'>
+          {wrappedData && <Wrapped data={wrappedData}/>}  // Pass the wrapped data to Wrapped component
+        </VStack>
+      </Center>
+    </>
+  );
+};
 
 export default Dash;
